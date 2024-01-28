6f154b85b767a0a4257fbb706e87e991945057dc
Kidongg <dlrlehd7@daum.net>
2024-01-28 22:26:55 +0900

    get server params

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,8 @@
-import { unstable_noStore } from "next/cache";
-import { Suspense } from "react";
+const Home = async ({ params, searchParams }: any) => {
+  console.log("params:", params);
+  console.log("searchParams: ", searchParams);
 
-const BlogPosts = async () => {
-  unstable_noStore();
-
-  const res = await fetch("https://api.vercel.app/blog");
-  // console.log("res: ", res);
-  const posts = await res.json();
-  // console.log("posts: ", posts);
-
-  return (
-    <ul>
-      {posts.map((post: any) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
-  );
-};
-
-const Home = () => {
-  return (
-    <section>
-      <h1>My blog</h1>
-      <Suspense fallback={"Loading..."}>
-        <BlogPosts />
-      </Suspense>
-    </section>
-  );
+  return <h1>Home</h1>;
 };
 
 export default Home;
